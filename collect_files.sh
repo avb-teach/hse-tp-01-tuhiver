50090f1ffd6c9b1c9d79c81d3466b89bcde6e938
tuhiver <midremina@edu.hse.ru>
2025-04-28 14:50:14 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-max_depth=-1
-positional_args=()
+depth=""
 input_dir="$1"   
 output_dir="$2"
-mkdir -p "$output_dir"
-if [[ "$1" == "--max_depth" ]]; then
-    max_depth="$2"
+if [ "$1" == "--max_depth" ]; then
+    depth="-maxdepth $2"
     shift 2
 fi
-if (( max_depth == -1 )); then
-    find "$input_dir" -type f -exec cp {} "$output_dir" \;
-else
-    find "$input_dir" -maxdepth "$max_depth" -type f -exec cp {} "$output_dir" \;
-fi
+mkdir -p "$output_dir"
+find "$input_dir" $depth -type f | while read file; do
+    name=$(basename "$file")
+    
+
+    if [ -f "$output_dir/$name" ]; then
+        i=1
+        while [ -f "$output_dir/${name%.*}_$i.${name##*.}" ]; do
+            ((i++))
+        done
+        name="${name%.*}_$i.${name##*.}"
+    fi
+    
+    
+    cp "$file" "$output_dir/$name"
+done
 
 exit 0
+
+
