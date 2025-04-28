9853d41845db4f20478e82b19bba37fcc607de24
tuhiver <midremina@edu.hse.ru>
2025-04-28 14:16:08 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -3,5 +3,14 @@ max_depth=-1
 positional_args=()
 input_dir="$1"   
 output_dir="$2"
-find "$input_dir" -type f -exec cp {} "$output_dir" \;
+if [[ "$1" == "--max_depth" ]]; then
+    max_depth="$2"
+    shift 2
+fi
+if (( max_depth == -1 )); then
+    find "$input_dir" -type f -exec cp {} "$output_dir" \;
+else
+    find "$input_dir" -maxdepth "$max_depth" -type f -exec cp {} "$output_dir" \;
+fi
+
 exit 0
