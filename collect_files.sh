1cd0612bcc1bcf0b9bfc758ce994e55ddcf6bf6a
tuhiver <midremina@edu.hse.ru>
2025-04-28 18:20:47 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-input_dir="$1"   
-output_dir="$2"
-find "$input_dir" -type f -exec cp {} "$output_dir" \;
+input_dir=$1
+output_dir=$2
+max_depth=$3
+
+
+mkdir -p "$output_dir"
+
+find "$input_dir" -maxdepth "$max_depth" -type f -exec cp --parents {} "$output_dir" \;
 exit 0
 
 
