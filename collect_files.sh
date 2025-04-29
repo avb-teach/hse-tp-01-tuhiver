f14e434f5a8ffa19b0f8a5d1f51efdfd72187108
tuhiver <midremina@edu.hse.ru>
2025-04-29 14:56:32 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 input_dir=$1
 output_dir=$2
-max_depth=$3
 
-
-if [ -z "$max_depth" ]; then
-    find "$input_dir" -type f -exec cp --parents {} "$output_dir" \;
-else
-    # Копируем с учетом максимальной глубины
-    find "$input_dir" -mindepth 1 -maxdepth "$max_depth" -type f -exec cp --parents {} "$output_dir" \;
-fi
+find "$input_dir" -type f -exec cp {} "$output_dir" \;
 
 exit 0
 
