651e3e5dc1ba445ca6253ca827457e0460e339cf
tuhiver <midremina@edu.hse.ru>
2025-04-30 20:55:27 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
+
 input_dir=$1
 output_dir=$2
-depth=$3
-find "$input_dir" -type f -exec cp {} "$output_dir" \;
+max_depth=$3
+
+
+
+mkdir -p "$output_dir"
+
+if [ -z "$max_depth" ]; then
+    find "$input_dir" -type f -exec cp --parents {} "$output_dir" \;
+else
+   
+    find "$input_dir" -type f | while read -r file; do
+        
+        rel_path="${file#$input_dir}"
+        rel_path="${rel_path#/}"
+        depth=$(echo "$rel_path" | grep -o '/' | wc -l)
+        if [ "$depth" -lt "$max_depth" ]; then
+         
+            mkdir -p "$output_dir/$(dirname "$rel_path")"
+            cp "$file" "$output_dir/$rel_path"
+        else
+            
+            prefix=$(dirname "$rel_path" | tr '/' '_')
+            cp "$file" "$output_dir/${prefix}_$(basename "$file")"
+        fi
+    done
+fi
 
 exit 0
 
